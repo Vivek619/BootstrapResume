71f2e9d2b73f3a2d5fa03f57f5046206a61c000f
Vivek619 <vivek111ee0204@gmail.com>
2019-09-26 02:45:22 +0530

    ok

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace new2_21Aug
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "new2_21Aug.Controllers" }
             );
         }
     }
